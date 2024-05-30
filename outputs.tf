d9062a5f590d907f8564ed3b92a49d7854587b95
Oleksandr Kuzminskyi <1763754+akuzminsky@users.noreply.github.com>
2024-05-29 17:25:22 -0700

    Export secret id (#3)
    Co-authored-by: Oleksandr Kuzminskyi <aleks@infrahouse.com>

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "secret_arn" {
   description = "ARN of the created secret"
   value       = aws_secretsmanager_secret.secret.arn
 }
+
+output "secret_id" {
+  description = "ID of the created secret"
+  value       = aws_secretsmanager_secret.secret.id
+}
