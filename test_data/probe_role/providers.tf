b4dd1a0f8dba0cc6e8ec01b702c8e45735ce9ceb
Oleksandr Kuzminskyi <aleksandr.kuzminsky@gmail.com>
2025-07-25 21:15:09 -0700

    Dynamic role, and self-hosted runners

--- test_data/probe_role/providers.tf
+++ test_data/probe_role/providers.tf
@@ -1,8 +1,11 @@
 provider "aws" {
-  assume_role {
-    role_arn = var.role_arn
-  }
   region = var.region
+  dynamic "assume_role" {
+    for_each = var.role_arn != null ? [1] : []
+    content {
+      role_arn = var.role_arn
+    }
+  }
   default_tags {
     tags = {
       "created_by" : "infrahouse/terraform-aws-secret" # GitHub repository that created a resource
